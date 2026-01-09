56b6e45101afeda18066f6d59cdaf332c9aabf01
DevAbidHasan <abidhasanplabon80@gmail.com>
2026-01-09 20:59:42 +0600

    installed tailwind, react-router and google-fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Planora</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 className='font-black jakarta my-10 opacity-60 text-amber-500 text-5xl text-center'>this is planora</h2>
+      
+      <p className='mx-10 text-gray-500 inter mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus laudantium tempore voluptates velit cumque quod magni excepturi repellendus enim omnis modi autem commodi officia quidem perferendis illo esse, ratione perspiciatis, adipisci ullam aperiam soluta accusantium! Quae ut officiis, assumenda, dolorem ipsam voluptates dolorum vel cupiditate deleniti sit iusto recusandae? Neque.</p>
+      <button class="btn btn-primary">Primary</button>
     </>
   )
 }
