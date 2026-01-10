e49b8db3e82e860a37a3b08ffd52e58a84572cde
DevAbidHasan <abidhasanplabon80@gmail.com>
2026-01-10 22:36:36 +0600

    added simple footer component

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,7 +3,46 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>this is footer</h2>
+             <footer className="flex flex-col bg-slate-50 items-center justify-around w-full py-16 text-sm text-gray-800/70">
+                <div className="flex items-center gap-8">
+                    <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
+                        Home
+                    </a>
+                    <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
+                        About
+                    </a>
+                    <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
+                        Services
+                    </a>
+                    <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
+                        Contact
+                    </a>
+                    <a href="#" className="font-medium text-gray-500 hover:text-black transition-all">
+                        Help
+                    </a>
+                </div>
+                <div className="flex items-center gap-4 mt-8 text-indigo-500">
+                    <a href="https://www.facebook.com/plabon48/" className="hover:-translate-y-0.5 transition-all duration-300" target='_blank'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </a>
+                    
+                    <a href="#" className="hover:-translate-y-0.5 transition-all duration-300">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-4 0v7h-4v-7a6 6 0 0 1 6-6M6 9H2v12h4zM4 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </a>
+                   
+                    <a href="#" className="hover:-translate-y-0.5 transition-all duration-300">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.4 5.4 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65S8.93 17.38 9 18v4" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            <path d="M9 18c-4.51 2-5-2-7-2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </a>
+                </div>
+                <p className="mt-8 text-center"> © 2026 Abid Hasan Plabon. All rights reservered.</p>
+            </footer>
         </div>
     );
 };
